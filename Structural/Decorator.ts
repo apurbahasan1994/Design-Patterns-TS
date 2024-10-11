56bf592a5366f13fe10431923d16a00ae4a19432
ApurbaHasan <apurbahasan1994@gmail.com>
2024-10-11 23:17:05 +0600

    added another implementation

--- Structural/Decorator.ts
+++ Structural/Decorator.ts
@@ -49,5 +49,53 @@ class MilkDecorator extends CoffeDecorator {
     }
 }
 
-let coffe : Coffe = new SimpleCoffee();
+let coffe: Coffe = new SimpleCoffee();
 coffe = new MilkDecorator(coffe);
+
+interface ServerRequest {
+    handle(request: any): void;
+}
+
+class BaseServer implements ServerRequest {
+    handle(request: any): void {
+        console.log('Handling request', request);
+    }
+
+}
+
+abstract class ServerRequestDecorator implements ServerRequest {
+    abstract handle(request: any): void;
+    constructor(protected serverRequest: ServerRequest) {
+
+    }
+
+}
+
+class LoggingMiddleWare extends ServerRequestDecorator {
+    handle(request: any): void {
+        console.log('Logging request', request);
+        this.serverRequest.handle(request);
+    }
+}
+
+
+class AuthMiddleWare extends ServerRequestDecorator {
+    handle(request: any): void {
+        if (request.isAuthenticated) {
+            console.log('req is Authenticated')
+        }
+        else {
+            console.log('Unauthorized');
+        }
+    }
+
+}
+
+const request = {
+    isAuthenticated: true,
+    body: "hello world"
+}
+
+let server = new BaseServer();
+server = new LoggingMiddleWare(server);
+server.handle(request);
